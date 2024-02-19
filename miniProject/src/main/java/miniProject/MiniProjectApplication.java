414030862877167ae8a3c42b797aa658896a108c
youare0118 <es5876@naver.com>
2024-02-19 16:30:46 +0900

    main.html 수정 중

--- miniProject/src/main/java/miniProject/MiniProjectApplication.java
+++ miniProject/src/main/java/miniProject/MiniProjectApplication.java
@@ -2,12 +2,21 @@ package miniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class MiniProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MiniProjectApplication.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(MiniProjectApplication.class, args);
+   }
+   
+   
+   @GetMapping("/")
+   public String index() {
+      return "thymeleaf/main";
+   }
 
 }
